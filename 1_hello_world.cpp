b39e43dcf9200416d42c42914dbf9a6cb66e59ed
Lin Myat Thein <linmyatthein@icloud.com>
2026-05-20 16:35:16 +0630

    hello world

---
+++ 1_hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    //This is a commennt. 
+     
+    /*
+    this is multiplle comment
+    */
+   
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "I am a student" << std::endl;
+
+    return 0;
+}
